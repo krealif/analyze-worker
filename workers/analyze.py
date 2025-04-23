3a62388d49947357e8d130182e2fa8e5c0b5f4e4
Shavana Alif <85614368+krealif@users.noreply.github.com>
2025-04-23 05:13:15 +0000

    update project

--- workers/analyze.py
+++ workers/analyze.py
@@ -5,11 +5,6 @@ from config.s3 import s3_client
 import uuid
 
 async def analyze_video(job: Job, token=None):
-    video_url = job.data.video_url
-
-    # download to tmp folder
-    s3_client.download_file(settings.S3_BUCKET, video_url, f"tmp/{video_url}")
-
     # fake job progress
     job_duration = 30
     total_steps = 10
@@ -20,11 +15,5 @@ async def analyze_video(job: Job, token=None):
         progress = int((i / total_steps) * 100)
         await job.updateProgress(progress)
         print(f"Job progress: {progress}%")
-
-    # upload file
-    key = f"uploads/{uuid.uuid4()}.json"
-    s3_client.upload_file("/path/upload", settings.S3_BUCKET, key)
-
-    print("Job completed.")
 
 print(uuid.uuid4());
