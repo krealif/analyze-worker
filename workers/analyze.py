2bdb48981437d9c30ee51d1df43c3724166c685a
Shavana Alif <85614368+krealif@users.noreply.github.com>
2025-04-23 14:26:30 +0700

    Update analyze.py

--- workers/analyze.py
+++ workers/analyze.py
@@ -5,15 +5,16 @@ from config.s3 import s3_client
 import uuid
 
 async def analyze_video(job: Job, token=None):
-    # fake job progress
-    job_duration = 30
+    print(f"Processing: {job.name}")
     total_steps = 10
 
     for i in range(total_steps + 1):
-        await asyncio.sleep(job_duration / total_steps)
+        await asyncio.sleep(5) # Sleep for 5 seconds
         
         progress = int((i / total_steps) * 100)
         await job.updateProgress(progress)
         print(f"Job progress: {progress}%")
+        
+    print(f"Job {job.name} completed.")
 
 print(uuid.uuid4());
